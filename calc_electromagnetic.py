a5d34ed7521003ca5f3a69a8c3cda300dab52496
DavidWalz <walz@physik.rwth-aachen.de>
2016-08-31 15:22:48 +0200

    calc_electromagnetic.py : fix lower end of s-tabulation

--- calc_electromagnetic.py
+++ calc_electromagnetic.py
@@ -8,9 +8,11 @@ me2 = 0.510998918e6**2  # squared electron mass [eV^2/c^4]
 sigmaThompson = 6.6524e-29  # Thompson cross section [m^2]
 alpha = 1. / 137.035999074  # fine structure constant
 
-# cross sections [1/m^2] for choosen s range [J^2]
 def sigmaPP(s):
-    """see Lee 1996"""
+    """
+    Pair production cross section (Bethe-Heitler), see Lee 1996.
+    Returns cross sections [1/m^2] for chosen s range [J^2].
+    """
     if (s < 4.*me2 * eV**2):
         return 0.
     else:
@@ -18,7 +20,10 @@ def sigmaPP(s):
         return sigmaThompson * 3./16.*(1-beta**2)*((3-beta**4)*log((1+beta)/(1-beta))-2*beta*(2-beta**2))
 
 def sigmaICS(s):
-    """see Lee 1996"""
+    """
+    Inverse Compton scattering cross sections, see Lee 1996.
+    Returns cross sections [1/m^2] for chosen s range [J^2].
+    """
     if (s < 1.0 * me2 *eV**2):
         return 0.
     else:
@@ -26,18 +31,24 @@ def sigmaICS(s):
       return sigmaThompson * 3./8. * me2 * eV**2 / s / beta * (2./beta/(1+beta)*(2.+2.*beta-beta**2-2.*beta**3)-1./beta**2 * (2.-3.*beta**2- beta**3)*log((1.+beta)/(1.-beta)))
 
 def sigmaTPP(s):
-    """see Lee 1996"""
+    """
+    Triplet-pair production cross section, see Lee 1996.
+    Returns cross sections [1/m^2] for chosen s range [J^2].
+    """
     if (28/9*log(s/me2 /eV**2)- 218./27. < 0.):
         return 0.
     else:
         return sigmaThompson *3.*alpha/8./pi*(28/9*log(s/me2 /eV**2)- 218./27.)
 
 def sigmaDPP(s):
-    """see R.W. Brown eq. (4.5) k**2 = q**2 = 0 case"""
+    """
+    Double-pair production cross section, see R.W. Brown eq. (4.5) with k^2 = q^2 = 0.
+    Returns cross sections [1/m^2] for chosen s range [J^2].
+    """
     if (s < 16 * me2 * eV **2):
         return 0.
     else:
-        # exponent = 1 instead of 6 will result in better reproduction of EleCa reference
+        # exponent = 1 instead of 6 results in better reproduction of EleCa data
         return 6.45*1e-34 *(1.- 16.*me2 *eV**2 / s)**6
 
 
@@ -63,8 +74,8 @@ def saveRate(E, sigma, skin, field, name):
 # because the integration carried out requires logspaced skin values.
 # Otherwise (logspace s from mass term to 10**23 eV and subtract mass term
 # to obtain skin) skin is not logspaced for small values.
-skin1 = logspace(-28, log10(10**23*eV**2), 1025)  # photons
-skin2 = logspace(-28, log10((10**23 - me2)*eV**2), 1025)  # electrons
+skin1 = logspace(log10(1E7*eV**2), log10( 10E23*eV**2), 1025)  # photons
+skin2 = logspace(log10(1E7*eV**2), log10((10E23-me2)*eV**2), 1025)  # electrons
 E = logspace(15, 23, 801) * eV  # energy range of interacting particle
 
 fields = [
@@ -105,8 +116,8 @@ def saveCDF(E, sigma, skin, field, name):
     fmt = '%.2f\t%.5e\t%.5e'
     savetxt(fname, data, fmt=fmt, header=header)
 
-skin1 = logspace(-28, log10(1e23 * eV**2), 500)
-skin2 = logspace(-28, log10((1e23 - me2)*eV**2), 500)
+skin1 = logspace(log10(1E7*eV**2), log10( 1E23*eV**2), 500)
+skin2 = logspace(log10(1E7*eV**2), log10((1E23-me2)*eV**2), 500)
 E = logspace(15, 23, 81) * eV  # energy range of interacting particle
 
 fields = [
@@ -126,14 +137,10 @@ for field in fields:
     saveCDF(E, sigmaTPP, skin2, field, 'EMTripletPairProduction_CDF')
 
 
-# consider different s-range for URB
-# important: tabulate skin in logspace and add resp. mass term to obtain s
-# because the integration carried out requires logspaced skin values.
-# Otherwise (logspace s from mass term to 10**23 eV and subtract mass term
-# to obtain skin) skin is not logspaced for small values.
+# Consider different s-range for URB
 field = photonField.URB_Protheroe96()
-skin1 = logspace(-28, log10(1e17 * eV**2), 500)
-skin2 = logspace(-28, log10((1e17 - me2)*eV**2), 500)
+skin1 = logspace(log10(1E7*eV**2), log10( 1E17 * eV**2), 500)
+skin2 = logspace(log10(1E7*eV**2), log10((1E17 - me2)*eV**2), 500)
 saveCDF(E, sigmaPP,  skin1, field, 'EMPairProduction_CDF')
 saveCDF(E, sigmaICS, skin2, field, 'EMInverseComptonScattering_CDF')
 saveCDF(E, sigmaTPP, skin2, field, 'EMTripletPairProduction_CDF')
